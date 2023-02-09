10dfabd68a3a4a03f7fe4c76bd348f4cd47feaa0
CodeurPerpetuelle <aypeez.aps@gmail.com>
2023-02-09 17:34:44 -0600

    Update MLmodel.py

--- MLmodel.py
+++ MLmodel.py
@@ -19,4 +19,39 @@ xgboost_container = sagemaker.image_uris.retrieve("xgboost", my_region, "latest"
 
 print("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + xgboost_container + " container for your SageMaker endpoint.")
 
+# Creating S3 bucket to store data
+
+bucket_name = 'your-s3-bucket-name' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
+s3 = boto3.resource('s3')
+try:
+    if  my_region == 'us-east-1':
+      s3.create_bucket(Bucket=bucket_name)
+    else: 
+      s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={ 'LocationConstraint': my_region })
+    print('S3 bucket created successfully')
+except Exception as e:
+    print('S3 error: ',e)
+    
+    
+
+ # Download the data to Sagemaker instance and load the data into the dataframe
+
+try:
+  urllib.request.urlretrieve ("https://d1.awsstatic.com/tmt/build-train-deploy-machine-learning-model-sagemaker/bank_clean.27f01fbbdf43271788427f3682996ae29ceca05d.csv", "bank_clean.csv")
+  print('Success: downloaded bank_clean.csv.')
+except Exception as e:
+  print('Data load error: ',e)
+
+try:
+  model_data = pd.read_csv('./bank_clean.csv',index_col=0)
+  print('Success: Data loaded into dataframe.')
+except Exception as e:
+    print('Data load error: ',e)
+    
+# Shuffle and split the data into training data and test data.
+train_data, test_data = np.split(model_data.sample(frac=1, random_state=1729), [int(0.7 * len(model_data))])
+print(train_data.shape, test_data.shape)
+
+# 
+
 
