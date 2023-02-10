94eec0f596f158cfb5415f7f09fefe6769da4606
CodeurPerpetuelle <aypeez.aps@gmail.com>
2023-02-09 18:04:45 -0600

    Update MLmodel.py

--- MLmodel.py
+++ MLmodel.py
@@ -1,4 +1,4 @@
-# import libraries
+# Importing relevant libraries
 import boto3, re, sys, math, json, os, sagemaker, urllib.request
 from sagemaker import get_execution_role
 import numpy as np
@@ -9,19 +9,17 @@ from IPython.display import display
 from time import gmtime, strftime
 from sagemaker.predictor import csv_serializer
 
-# Define IAM role
+# Defining IAM role
 role = get_execution_role()
 prefix = 'sagemaker/DEMO-xgboost-dm'
 my_region = boto3.session.Session().region_name # set the region of the instance
 
-# this line automatically looks for the XGBoost image URI and builds an XGBoost container.
+# This line automatically looks for the XGBoost image URI and builds an XGBoost container.
 xgboost_container = sagemaker.image_uris.retrieve("xgboost", my_region, "latest")
-
 print("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + xgboost_container + " container for your SageMaker endpoint.")
 
 # Creating S3 bucket to store data
-
-bucket_name = 'your-s3-bucket-name' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
+bucket_name = 'peizo-s3-bucket' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
 s3 = boto3.resource('s3')
 try:
     if  my_region == 'us-east-1':
@@ -32,26 +30,74 @@ try:
 except Exception as e:
     print('S3 error: ',e)
     
-    
-
- # Download the data to Sagemaker instance and load the data into the dataframe
-
+# Download the data to Sagemaker instance 
 try:
   urllib.request.urlretrieve ("https://d1.awsstatic.com/tmt/build-train-deploy-machine-learning-model-sagemaker/bank_clean.27f01fbbdf43271788427f3682996ae29ceca05d.csv", "bank_clean.csv")
   print('Success: downloaded bank_clean.csv.')
 except Exception as e:
   print('Data load error: ',e)
 
+# Load the data into the dataframe
 try:
   model_data = pd.read_csv('./bank_clean.csv',index_col=0)
   print('Success: Data loaded into dataframe.')
 except Exception as e:
-    print('Data load error: ',e)
+  print('Data load error: ',e)
     
 # Shuffle and split the data into training data and test data.
 train_data, test_data = np.split(model_data.sample(frac=1, random_state=1729), [int(0.7 * len(model_data))])
 print(train_data.shape, test_data.shape)
 
-# 
+# Training the model. This code reformats the header and first column of the training data and then loads the data from the S3 bucket. 
+# This step is required to use the Amazon SageMaker pre-built XGBoost algorithm.
 
+pd.concat([train_data['y_yes'], train_data.drop(['y_no', 'y_yes'], axis=1)], axis=1).to_csv('train.csv', index=False, header=False)
+boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/train.csv')).upload_file('train.csv')
+s3_input_train = sagemaker.inputs.TrainingInput(s3_data='s3://{}/{}/train'.format(bucket_name, prefix), content_type='csv')
+
+# Setting up the Amazon SageMaker session, creating an instance of the XGBoost model (an estimator), and defining the model’s hyperparameters.
+
+sess = sagemaker.Session()
+xgb = sagemaker.estimator.Estimator(xgboost_container,role, instance_count=1, instance_type='ml.m4.xlarge',output_path='s3://{}/{}/output'.format(bucket_name, prefix),sagemaker_session=sess)
+xgb.set_hyperparameters(max_depth=5,eta=0.2,gamma=4,min_child_weight=6,subsample=0.8,silent=0,objective='binary:logistic',num_round=100)
+
+# Starting the training job
+# This code trains the model using gradient optimization on a ml.m4.xlarge instance.
+
+xgb.fit({'train': s3_input_train})
+
+# Deploying the model
+# In this step I deploy the trained model to an endpoint, reformat and load the CSV data, then run the model to create predictions.
+
+xgb_predictor = xgb.deploy(initial_instance_count=1,instance_type='ml.m4.xlarge')
+
+# To predict whether customers in the test data enrolled for the bank product or not
+
+from sagemaker.serializers import CSVSerializer
+
+test_data_array = test_data.drop(['y_no', 'y_yes'], axis=1).values #load the data into an array
+xgb_predictor.serializer = CSVSerializer() # set the serializer type
+predictions = xgb_predictor.predict(test_data_array).decode('utf-8') # predict!
+predictions_array = np.fromstring(predictions[1:], sep=',') # and turn the prediction into an array
+print(predictions_array.shape)
+
+# Evaluating the model performance
+
+cm = pd.crosstab(index=test_data['y_yes'], columns=np.round(predictions_array), rownames=['Observed'], colnames=['Predicted'])
+tn = cm.iloc[0,0]; fn = cm.iloc[1,0]; tp = cm.iloc[1,1]; fp = cm.iloc[0,1]; p = (tp+tn)/(tp+tn+fp+fn)*100
+print("\n{0:<20}{1:<4.1f}%\n".format("Overall Classification Rate: ", p))
+print("{0:<15}{1:<15}{2:>8}".format("Predicted", "No Purchase", "Purchase"))
+print("Observed")
+print("{0:<15}{1:<2.0f}% ({2:<}){3:>6.0f}% ({4:<})".format("No Purchase", tn/(tn+fn)*100,tn, fp/(tp+fp)*100, fp))
+print("{0:<16}{1:<1.0f}% ({2:<}){3:>7.0f}% ({4:<}) \n".format("Purchase", fn/(tn+fn)*100,fn, tp/(tp+fp)*100, tp))
+
+# Clean Up 
+# Delete your endpoint
+xgb_predictor.delete_endpoint(delete_endpoint_config=True)
+
+# Delete your training artifacts and S3 bucket
+bucket_to_delete = boto3.resource('s3').Bucket(bucket_name)
+bucket_to_delete.objects.all().delete()
+
+# Finally Delete your SageMaker Notebook
 
